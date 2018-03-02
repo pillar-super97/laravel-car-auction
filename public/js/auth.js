257b0197eb5c82658dd120a5a5f0dc52d0a04785
stefan_jevtic <sjevtic14@gmail.com>
2018-03-02 22:55:27 +0100

    ajax validation started

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,17 +1,22 @@
-// $(document).ready( () => {
-//     $('.btn-Login').click( () => {
-//         let username = $('.tbUsername').val();
-//         let password = $('.tbPassword').val();
-//         $.ajax({
-//             headers: {
-//                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//             },
-//             type: 'post',
-//             url: '/login',
-//             data:{username, password},
-//             success(data){
-//                 alert(data);
-//             }
-//         })
-//     })
-// })
+$(document).ready( () => {
+    $('.btn-Login').click( () => {
+        let username = $('.tbUsername').val();
+        let password = $('.tbPassword').val();
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $.ajax({
+            type: 'post',
+            url: '/login',
+            data:JSON.stringify({username, password}),
+            success(data){
+                alert(data);
+            }
+        })
+    })
+})
+
+// AJAX Requests & Validation
+// In this example, we used a traditional form to send data to the application. However, many applications use AJAX requests. When using the validate method during an AJAX request, Laravel will not generate a redirect response. Instead, Laravel generates a JSON response containing all of the validation errors. This JSON response will be sent with a 422 HTTP status code.
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Scarica gratis GARAGE Template html/css - Web Domus Italia - Web Agency </title>
     <meta name="description" content="Scarica gratis il nostro Template HTML/CSS GARAGE. Se avete bisogno di un design per il vostro sito web GARAGE può fare per voi. Web Domus Italia">
-    <meta name="_token" content="{!! csrf_token() !!}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="Web Domus Italia">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
